e7dd010b610bc647fb23f63072aac19b54e714b8
jocelyn-apple <jocelyn.apple@cuw.edu>
2022-05-02 10:30:23 -0500

    chomp movement

---
+++ Assets/ChompScript.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChompScript : MonoBehaviour
+{
+    private Rigidbody rb;
+    public float speed = 5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = this.gameObject.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("up"))
+        {
+            rb.velocity = Vector3.forward * speed;
+        }
+        else if (Input.GetKeyDown("down"))
+        {
+            rb.velocity = Vector3.back * speed;
+        }
+        else if (Input.GetKeyDown("left"))
+        {
+            rb.velocity = Vector3.left * speed;
+        }
+        else if (Input.GetKeyDown("right"))
+        {
+            rb.velocity = Vector3.right * speed;
+        }
+        else if (Input.GetKeyDown("space"))
+        {
+            rb.velocity = Vector3.up * speed;
+        }
+    }
+}
